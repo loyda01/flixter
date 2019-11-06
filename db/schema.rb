980b97fa1beb9764d6ed48686aec947be369603c
Loyda Lopez <loydalopez1@gmail.com>
2019-11-06 02:04:34 +0000

    fixing images

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_11_03_215744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_courses_on_user_id"
+    
   end
 
   create_table "lessons", force: :cascade do |t|
